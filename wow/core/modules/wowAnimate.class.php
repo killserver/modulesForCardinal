66a37637dc7b9f49aebfa71f95b868431e7c2f64
killserver <killeserver@gmail.com>
2017-11-22 09:47:20 +0200

    add json and update wow.js module

--- wow/core/modules/wowAnimate.class.php
+++ wow/core/modules/wowAnimate.class.php
@@ -4,6 +4,7 @@ class wowAnimate extends modules {
 	
 	function __construct() {
 		$this->manifest_set(array("create_js", "full"), array("{C_default_http_local}js/wow.min.js"));
+		$this->manifest_set(array("create_css", "full"), array("{C_default_http_local}js/animate.min.css"));
 	}
 	
 }
