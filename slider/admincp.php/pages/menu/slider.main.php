e288eeaa6c4a363c5c65b66c8760ec83d558ee99
killserver <killeserver@gmail.com>
2017-11-24 16:45:05 +0200

    add slider

---
+++ slider/admincp.php/pages/menu/slider.main.php
@@ -0,0 +1,16 @@
+<?php
+$links['slider']["cat"][] = array(
+'link' => "{C_default_http_host}".(!defined("ADMINCP_DIRECTORY") ? "admincp.php" : ADMINCP_DIRECTORY)."/?pages=Archer&type=slider",
+'title' => "{L_\"Слайдер\"}",
+'type' => "cat",
+'access' => true,
+'icon' => 'fa-sliders',
+);
+$links['slider']["item"][] = array(
+'link' => "{C_default_http_host}".(!defined("ADMINCP_DIRECTORY") ? "admincp.php" : ADMINCP_DIRECTORY)."/?pages=Archer&type=slider",
+'title' => "{L_\"Слайдер\"}",
+'type' => "item",
+'access' => true,
+'icon' => '',
+);
+?>
