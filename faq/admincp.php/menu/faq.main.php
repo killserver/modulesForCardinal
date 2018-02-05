8972f84cc2cbdf5634619cf1a77fcec7ccb290bc
killserver <killeserver@gmail.com>
2018-02-05 17:18:47 +0200

    update f.a.q.

---
+++ faq/admincp.php/menu/faq.main.php
@@ -0,0 +1,16 @@
+<?php
+$links['faq']["cat"][] = array(
+'link' => "{C_default_http_host}{D_ADMINCP_DIRECTORY}/?pages=Archer&type=faq",
+'title' => "F.A.Q.",
+'type' => "cat",
+'access' => true,
+'icon' => 'fa-spin fa-bug',
+);
+$links['faq']["item"][] = array(
+'link' => "{C_default_http_host}{D_ADMINCP_DIRECTORY}/?pages=Archer&type=faq",
+'title' => "F.A.Q.",
+'type' => "item",
+'access' => true,
+'icon' => '',
+);
+?>
